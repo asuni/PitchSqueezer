afb80e3e22217c9489c49060c5bdd58feb62e0ef
Antti Suni <antti.suni@helsinki.fi>
2023-11-23 23:01:07 +0200

    Update test_keele.py

--- test/test_keele.py
+++ test/test_keele.py
@@ -9,141 +9,87 @@ import amfm_decompy.basic_tools as basic
 import parselmouth
 import soundfile as sf
 import os
-from pygame import mixer
 
-def _highlight(f0s, index):
-    for line in plt.gca().lines:
-        line.remove()
-    colors = ["green", "blue", "red", "purple"]
-    for i in range(len(f0s)):
-        f0s[i][(f0s[i]==0)] = np.nan
-        if i == index:
-            plt.plot(f0s[i], color=colors[i], linewidth=2)
-        else:
-            plt.plot(f0s[i], color=colors[i],alpha=0.15)
-        plt.pause(0.1)
-
-def play(wav):
+def get_errors(test_f0, ref_f0):
     
-    def play_wav(wav):
-        mixer.music.load(wav)
-        mixer.music.play()
-   
-    play_wav(wav)
-    input("press ENTER to stop playback")
-    mixer.music.stop()
-   
-   
-def vde(test_f0, ref_f0):
+    voicing_errors = np.logical_xor(ref_f0 == 0, test_f0 == 0)
+    voicing_errors = np.sum(voicing_errors)/len(ref_f0)*100
+    voiced_frames = np.logical_and(ref_f0 > 0, test_f0 > 0)
+    test_f0 = test_f0[voiced_frames]
+    ref_f0 = ref_f0[voiced_frames]
+    errors = np.abs(test_f0 - ref_f0)
+    gross_errors = errors > 0.2 * ref_f0
+    percentage_gross_errors = (np.sum(gross_errors) / len(errors)) * 100
+    fine_errors = errors[~gross_errors]
+    mean_fine_errors = np.mean(fine_errors)
+    return voicing_errors, percentage_gross_errors, mean_fine_errors
+
+
+def analysis(wav, fmin, fmax, method = "squeezer"):
     
-    test_voiced = test_f0 > 0
-    ref_voiced = ref_f0 > 0
-
-    # Step 3: Calculate Voicing Decision Error
-    TP = np.sum(np.logical_and(test_voiced, ref_voiced))
-    TN = np.sum(np.logical_and(~test_voiced, ~ref_voiced))
-    FP = np.sum(np.logical_and(test_voiced, ~ref_voiced))
-    FN = np.sum(np.logical_and(~test_voiced, ref_voiced))
-
-    # Step 4: Calculate Voicing Error Metrics
-    VDE = (FP + FN) / (TP + TN + FP + FN)
-    return VDE
-def mae(test_f0, ref_f0):
-    test_voiced = test_f0 > 0
-    ref_voiced = ref_f0 > 0
-    voiced_frames = np.logical_and(test_voiced, ref_voiced)
-
-    return np.mean(np.abs(test_f0[voiced_frames]-ref_f0[voiced_frames]))
-   
-def gross(test_f0, ref_f0, voicing_threshold=0, deviation_percentage=20):
-  
-    test_voiced = test_f0 > voicing_threshold
-    ref_voiced = ref_f0 > voicing_threshold
-
-   
-    voiced_frames = np.logical_and(test_voiced, ref_voiced)
-
-    # Step 3: Identify frames with significant pitch deviation (>20%)
-    deviation_threshold = deviation_percentage / 100.0
-    pitch_deviation = np.abs(test_f0[voiced_frames] - ref_f0[voiced_frames]) / ref_f0[voiced_frames]
+    if method == "squeezer":
+        f0_ps, if0_ps = ps.track_pitch(wav, min_hz=fmin, max_hz=fmax, voicing_thresh=0.4,frame_rate=100)
+        return f0_ps
     
-    gross_errors = np.sum(pitch_deviation > deviation_threshold) / np.sum(voiced_frames)
+    elif method == "yaapt":
+        signal = basic.SignalObj(wav)
+        f0_pyaapt = pYAAPT.yaapt(signal, **{'f0_min' : fmin, 'f0_max':fmax, 'frame_space' : 10.0})
+        f0_pyaapt = f0_pyaapt.samp_values
+        f0_pyaapt = np.pad(f0_pyaapt, 1, 'edge')
+        return f0_pyaapt
    
-    return gross_errors*100
+    elif method == "pyin":
+        x, fs = librosa.load(wav, sr=None)
+       
+        f0_pyin, voiced_flag, voiced_probs = librosa.pyin(x,sr=fs, fmin=fmin, fmax=fmax, frame_length=int(fs/16), hop_length = int(fs/100))
+        f0_pyin[np.isnan(f0_pyin)]=0
+        return f0_pyin
 
-def analysis(wav, fmin, fmax, thresh):
+    elif method == "praat":
+        sound = parselmouth.Sound(wav)
+        pitch = sound.to_pitch_ac(time_step = 0.01, pitch_floor=fmin, pitch_ceiling=fmax)
+        pitch = pitch.selected_array['frequency']
+        pitch = np.pad(pitch, 2, 'edge')
+        return pitch
     
-
-  
-    f0_ps, if0_ps = ps.track_pitch(wav, voicing_thresh=thresh, min_hz=fmin, max_hz=fmax,target_rate=100)
-  
-    return f0_ps
-    signal = basic.SignalObj(wav)
-    f0_pyaapt = pYAAPT.yaapt(signal, **{'f0_min' : fmin, 'f0_max':fmax, 'frame_space' : 10.0})
-    f0_pyaapt = f0_pyaapt.samp_values
-    return f0_pyaapt
+    else:
+        print(method+" not implemented")
+        return None
     
-    x, fs = librosa.load(wav, sr=None)
-    f0_pyin, voiced_flag, voiced_probs = librosa.pyin(x,sr=fs, fmin=fmin, fmax=fmax, hop_length = int(fs/100))
-  
-    return(f0_pyin)
-    ref[ref==0] = np.nan
-
-    ref[ref<120] = np.nan
-    f0_ps[f0_ps==0] = np.nan
-    plt.plot(f0_ps, label="squeezer")
-    #plt.plot(f0_pyin, label="pyin")
-    plt.plot(ref, label="ref")
-    plt.legend()
-    plt.show()
-    return
-    start_time = time.time()
-    x, fs = librosa.load(wav, sr=None)
-    f0_pyin, voiced_flag, voiced_probs = librosa.pyin(x,sr=fs, fmin=fmin, fmax=fmax, hop_length = int(fs/200))
-    print("pyin analysis done in ", time.time()-start_time, "seconds")
-    start_time = time.time()
-
-    signal = basic.SignalObj(wav)
-    f0_pyaapt = pYAAPT.yaapt(signal, **{'f0_min' : fmin, 'f0_max':fmax, 'frame_space' : 5.0})
-    f0_pyaapt = f0_pyaapt.samp_values
-   
-    print("pYAAPT analysis done in ", time.time()-start_time, "seconds")
-
-
   
     
 if __name__ == "__main__":
-    import sys, glob
+    import sys, glob, time
     test_files = sorted(glob.glob(sys.argv[1]+"/*/signal.wav"))
     ref_files = sorted(glob.glob(sys.argv[1]+"/*/*.npy"))
 
-    import random
-    #random.shuffle(test_files)
-    plt.figure(figsize=(12,4))
-    plt.ylim(50,500)
-    mixer.init()
-    refs = []
-    tests = []
-    for f, r in zip(test_files, ref_files):
-        print("analyzing "+f)
+  
+    for method in ("praat", "squeezer", "yaapt", "pyin"):
+    
+        print("evaluating "+method,end=": ", flush=True)
+        start = time.time()
+        refs = []
+        tests = []
+        for f, r in zip(test_files, ref_files):
+            print(".", end="",flush=True)
+            #print("analyzing "+f)
 
-        ref = np.load(r)
-        ref_f0 = np.array([item[1] for item in ref])
-        ref_f0[ref_f0<60]=0
-        f0 = analysis(f, 50, 400, 0.2)
-        #try:
-        f0 = f0[:len(ref_f0)]
-        #except:
-        ref_f0 = ref_f0[:len(f0)]
-        print("vde:",vde(f0, ref_f0))
-        plt.plot(f0, 'red')
-        plt.plot(ref_f0, 'black')
-        plt.show()
-        refs.append(ref_f0)
-        tests.append(f0)
-    ref_f0= np.concatenate(refs, axis=None)
-    test_f0= np.concatenate(tests, axis=None)
+            ref = np.load(r)
+            ref_f0 = np.array([item[1] for item in ref])
+            ref_f0[ref_f0<60]=0
+            f0 = analysis(f, 50, 500, method)
+            # match lengths (for all methods, 1 frame off only)
+            f0 = f0[:len(ref_f0)]
+            ref_f0 = ref_f0[:len(f0)]
+            
+            refs.append(ref_f0)
+            tests.append(f0)
 
-    print("vde:",vde(test_f0, ref_f0))
-    print("gross:",gross(test_f0, ref_f0))
-    print("mae:",mae(test_f0, ref_f0))
+        ref_f0= np.concatenate(refs, axis=None)
+        test_f0= np.concatenate(tests, axis=None)
+
+        vde, gpe, fpe = get_errors(test_f0, ref_f0)
+        print(" done in ", time.time()-start, "seconds")
+        print("vde " +str(vde)+"% gpe "+str(gpe)+"% fpe "+str(fpe)+"hz\n")
+  
+   
